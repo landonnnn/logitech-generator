74baf121e084e13d22fd3a80e2516a23836fe35c
Landon <34496757+landonnnn@users.noreply.github.com>
2019-12-30 19:57:03 -0600

    Update application.php

--- application.php
+++ application.php
@@ -4,8 +4,8 @@
 	$SerialToCheck2 = "1503LZ0D" . chr(rand(65,90)) . chr(rand(65,90)) . chr(rand(65,90)) . "8"; // XXX8
 	$SerialToCheck3 = "1546MR0A" . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9); //XXXX
 	$SerialArray = array($SerialToCheck, $SerialToCheck1, $SerialToCheck2, $SerialToCheck3);
-    $randomSerial = $SerialArray[array_rand($SerialArray)];
-    echo "<p>Trying: " . $randomSerial . "</p>";
+	$randomSerial = $SerialArray[array_rand($SerialArray)];
+	echo "<p>Trying: " . $randomSerial . "</p>";
 	$url = "https://support.logitech.com/apexremote";
 	$data = "{\n\t\"action\": \"SupportFindProductRegistrationController\",\n\t\"method\": \"fetchProductFromSerial\",\n\t\"data\": [\n\t\t\"" . $randomSerial . "\",\n\t\t\"en_us\"\n\t],\n\t\"type\": \"rpc\",\n\t\"tid\": 5,\n\t\"ctx\": {\n\t\t\"csrf\": \"blah\",\n\t\t\"vid\": \"\",\n\t\t\"ns\": \"\",\n\t\t\"ver\": 30\n\t}\n}";
 	$data_string = json_encode($data);
@@ -16,12 +16,12 @@
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 		'Content-Type:application/json',
 		'referer: https://support.logitech.com/en_us/register'
-   	));
-   	curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-   	curl_setopt($ch, CURLOPT_VERBOSE, 1);
-   	$result= curl_exec ($ch);
-   	echo $result;
-   	curl_close($ch);
+	));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+	curl_setopt($ch, CURLOPT_VERBOSE, 1);
+	$result= curl_exec ($ch);
+	echo $result;
+	curl_close($ch);
 ?>
 <!DOCTYPE html>
 <html lang="en">
